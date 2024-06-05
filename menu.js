82cb9d96bcd5e85cd4304a083a1934341571c2c8
DidkivskaOlena <didkivskalyona@gmail.com>
2024-06-05 13:58:00 +0300

    fix phone

--- menu.js
+++ menu.js
@@ -5,6 +5,8 @@
         closeMenuForLink1: document.querySelector('.l1'),
         closeMenuForLink2: document.querySelector('.l2'),
         closeMenuForLink3: document.querySelector('.l3'),
+        closeMenuForPhone1: document.querySelector('.c1'),
+        closeMenuForPhone2: document.querySelector('.c2'),
         menu: document.querySelector('#menu'),
         body: document.querySelector('body'),
       };
@@ -14,6 +16,8 @@
       refs.closeMenuForLink1.addEventListener('click', toggleMenuInput);
       refs.closeMenuForLink2.addEventListener('click', toggleMenuInput);
       refs.closeMenuForLink3.addEventListener('click', toggleMenuInput);
+      refs.closeMenuForPhone1.addEventListener('click', toggleMenuInput);
+      refs.closeMenuForPhone2.addEventListener('click', toggleMenuInput);
       
       function toggleMenu() {
         refs.menu.classList.toggle('is-hidden');
