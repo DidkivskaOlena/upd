50c24e589138ab62460dde9e26c9c993275c483e
DidkivskaOlena <didkivskalyona@gmail.com>
2024-06-06 19:39:07 +0300

    fix menu

--- menu.js
+++ menu.js
@@ -1,6 +1,8 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileMenuContentRef = document.querySelector('[data-menu-content]');
+
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -10,5 +12,20 @@
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');
+    mobileMenuContentRef.classList.toggle('is-open');
+    document.body.classList.toggle("modal-open");
+
   });
+
+  mobileMenuContentRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    mobileMenuContentRef.classList.toggle('is-open');
+    document.body.classList.toggle("modal-open");
+  })
 })();
