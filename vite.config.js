2700632d8ca311428f63aa39b46565aa1a58e08b
DidkivskaOlena <didkivskalyona@gmail.com>
2024-10-03 09:26:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/public/',
+  build: {
+    rollupOptions: {
+      // external: [/^\/external-assets\/.*$/],
+    },
+  },
 });
